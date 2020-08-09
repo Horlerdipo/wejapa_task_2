cd4bf99265b53c1eff0ae7a452df52b2cb575881
Horlerdipo <oladipoumar@gmail.com>
2020-08-09 12:02:03 +0100

    Fixed javascript validation

--- index.php
+++ index.php
@@ -24,29 +24,29 @@
                             <form class="col s12" action="submit.php" method="POST">
                                 <div class="row">
                                     <div class="input-field col s12 m6">
-                                        <input id="first_name" type="text" required class="validate" name="first_name">
+                                        <input id="first_name" type="text" class="validate" name="first_name">
                                         <label for="first_name">First Name</label>
                                     </div>
                                     <div class="input-field col s12 m6">
-                                        <input id="last_name" type="text" required class="validate" name="second_name">
+                                        <input id="last_name" type="text" class="validate" name="second_name">
                                         <label for="last_name">Second Name</label>
                                     </div>
                                 </div>
 
                                 <div class="row">
                                     <div class="input-field col s12 m6">
-                                        <input id="email" type="email" required class="validate" name="email">
+                                        <input id="email" type="email" class="validate" name="email">
                                         <label for="email">Email</label>
                                     </div>
                                     <div class="input-field col s12 m6">
-                                        <input id="date" type="text" required class="validate datepicker" name="birthday">
+                                        <input id="date" type="text" class="validate datepicker" name="birthday">
                                         <label for="date">Birthday</label>
                                     </div>
                                 </div>
 
                                 <div class="row">
                                     <div class="input-field col s12 m6">
-                                        <input id="color" type="color" required class="validate color-picker" name="color">
+                                        <input id="color" type="color" class="validate color-picker" name="color">
                                         <label for="color">Favorite Color</label>
                                         <span class="helper-text">This will be the color of your page</span>
                                     </div>
@@ -74,12 +74,12 @@
 
                                 <div class="row">
                                     <div class="input-field col s12 m6">
-                                        <input id="password" type="password" name="password" required class="validate">
+                                        <input id="password" type="password" name="password" class="validate">
                                         <label for="password">Password</label>
                                     </div>
 
                                     <div class="input-field col s12 m6">
-                                        <select class="validate" required name="department">
+                                        <select class="validate" name="department">
                                             <option value="" disabled selected>Choose your option</option>
                                             <option value="1">IT</option>
                                             <option value="2">HR</option>
@@ -88,7 +88,7 @@
                                         <label>Department</label>
                                     </div>
                                 </div>
-                                <button type="submit" class="btn ">Register</button>
+                                <button type="submit" class="btn">Register</button>
                             </form>
                         </div>
                     </div>
@@ -100,6 +100,7 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="index.js"></script>
     <script>
         $(document).ready(() => {
             $('.datepicker').datepicker({
@@ -118,86 +119,6 @@
             <?php endif ?>
             <?php session_destroy() ?>
         });
-
-
-        $('.check input:checkbox').click(function() {
-            $('.check input:checkbox').not(this).prop('checked', false);
-        });
-
-        $(document).ready(() => {
-            $('select').formSelect();
-        });
-
-        $(document).ready(() => {
-            $("form").submit((e) => {
-                validate(e);
-            });
-        });
-
-        function validate(e) {
-            //CHECK IF GENDER WAS SELECTED
-            let gender_check = $('input[name="gender[]"]:checked').length == 0;
-            if (gender_check) {
-                e.preventDefault();
-                M.toast({
-                    html: 'You have to select a gender!',
-                    classes: 'dark-text red darken-4'
-                });
-            }
-
-
-            let password = $('input[name="password"]').val();
-
-            //CHECK IF PASSWORD LENGTH IS UP TO 15
-            if (password.length < 14) {
-                e.preventDefault();
-                console.log(password);
-                M.toast({
-                    html: 'Your Password must have at least 15 characters!',
-                    classes: 'dark-text red darken-4'
-                });
-
-            }
-            //CHECK IF PASSWORD CONTAINS UPPERCASE
-            let upperCase = new RegExp('[A-Z]');
-            if (!password.match(upperCase)) {
-                e.preventDefault();
-                M.toast({
-                    html: 'Your Password must contain uppercase letters!',
-                    classes: 'dark-text red darken-4'
-                });
-            }
-
-            //CHECK IF PASSWORD CONTAINS LOWERCASE
-            let lowerCase = new RegExp('[a-z]');
-            if (!password.match(lowerCase)) {
-                e.preventDefault();
-                M.toast({
-                    html: 'Your Password must contain lower letters!',
-                    classes: 'dark-text red darken-4'
-                });
-            }
-
-            //CHECK IF PASSWORD CONTAINS NUMBERS
-            let number = new RegExp('[0-9]');
-            if (!password.match(number)) {
-                e.preventDefault();
-                M.toast({
-                    html: 'Your Password must contain numbers!',
-                    classes: 'dark-text red darken-4'
-                });
-            }
-
-            //CHECK IF PASSWORD CONTAINS SPECIAL CHARACTER
-            let special = new RegExp('[^a-zA-Z0-9]');
-            if (!password.match(special)) {
-                e.preventDefault();
-                M.toast({
-                    html: 'Your Password must contain special characters!',
-                    classes: 'dark-text red darken-4'
-                });
-            }
-        }
     </script>
 </body>
 
